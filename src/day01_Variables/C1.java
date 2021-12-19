5d959a3dc06bb9ea736e45c72f03ce472672a3c4
unknown <flzgltr11@gmail.com>
2021-12-19 20:05:38 +0100

    Variables

---
+++ src/day01_Variables/C1.java
@@ -0,0 +1,47 @@
+package day01_Variables;
+
+public class C1 {
+
+	public static void main(String[] args) {
+		
+
+
+	
+		int sayi1=10;
+		int sayi2=20;
+		
+		System.out.println(sayi1+sayi2);
+		
+		String sayi3="10";
+		String sayi4="20";
+		System.out.println(sayi3 + sayi4);
+		
+		String sayi5="25";
+		String sayi6="28";
+		
+		int sayi7=30;
+		
+		System.out.println(sayi7+"sayi6+sayi5");
+		System.out.println(sayi5+sayi6+sayi7);
+		String harf1="B";
+		char harf2='B';
+		
+		System.out.println("harf1+harf1"+" " + harf1 + "\n "+harf2);
+		
+		
+		
+		
+		
+		
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	}
+
+
