cff9a50a2d2c32b75b48ceff4914058f422788b4
unknown <flzgltr11@gmail.com>
2021-12-26 21:58:03 +0100

    ForEach

---
+++ src/Arrays/C02.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class C02 {
+
+	public static void main(String[] args) {
+		
+		// Bir integer array olusturunuz 
+				// ve bu array’deki tum sayilarin carpimini For-each loop kullanarak bulunuz. 
+				// Sonucu ekrana yazdiriniz.
+				
+				int arr[]= {3,4,2,5};
+				
+				int carpim=1;
+				
+				for (int each : arr) {
+					
+					carpim *= each;
+					
+				}
+				
+				System.out.println("Arrayin elemanlari carpimi : " + carpim);
+
+
+		
+		
+
+	}
+
+}
